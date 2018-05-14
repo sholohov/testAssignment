54ec91a387bddb63d771c48891c2a30217799abc
sholhov <asolohov@gmail.com>
2018-05-14 08:17:55 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -39,5 +39,17 @@ function contentMarginTop() {
 
 function scrollBtnInit() {
 	let btn = document.querySelector('#move-to-top');
-	btn.addEventListener('click', Tools.scrollPageToTop);
+	btn.addEventListener('click', function scrollPageToTop() {
+		console.warn("WWWW");
+		function getScroll() {
+			return document.documentElement.scrollTop;
+		}
+		function run() {
+			if (getScroll() > 0) {
+				window.scrollBy(0, -Math.max((getScroll() / 6), 1));
+				requestAnimationFrame(run);
+			}
+		}
+		run();;
+	});
 }
